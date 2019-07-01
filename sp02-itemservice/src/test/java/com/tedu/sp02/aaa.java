287ff5e50fa4885ff40451d90d194307e78ee9aa
全日制 <@DESKTOP-J9KSVUM>
2019-07-01 09:34:36 +0800

    MQ

--- sp02-itemservice/src/test/java/com/tedu/sp02/aaa.java
+++ sp02-itemservice/src/test/java/com/tedu/sp02/aaa.java
@@ -1,16 +1,15 @@
 package com.tedu.sp02;
 
+import org.bouncycastle.crypto.MaxBytesExceededException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class Sp02ItemserviceApplicationTests {
+import com.jayway.jsonpath.internal.function.numeric.Max;
 
-	@Test
-	public void contextLoads() {
-	}
+public interface aaa {
+
 
 }
+abstract class bbb{}
